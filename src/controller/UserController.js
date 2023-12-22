550f2ce81cd70d8d6aad3039bd5e018c2838ee0a
Gabriel Tangerina <gabriel.tangerina@hotmail.com>
2023-12-22 10:32:37 -0300

    Adicionando métodos CRUD

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,16 +1,90 @@
 import User from '../models/User';
 
-class HomeController {
+class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+
+      await user.destroy();
+      return res.json('Usuário excluido com sucesso');
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
   }
 }
 
-export default new HomeController();
+export default new UserController();
