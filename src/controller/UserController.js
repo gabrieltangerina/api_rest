cf46e397241409fd288ff78f1f053013a4c0fcad
Gabriel Tangerina <gabriel.tangerina@hotmail.com>
2023-12-23 00:19:10 -0300

    Arrumando dados de retorno da API

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -4,7 +4,8 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -14,7 +15,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] }); // Para pegar somente esses atributos id, nome e email
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -23,10 +24,11 @@ class UserController {
 
   async show(req, res) {
     try {
-      const { id } = req.params;
+      // const { id } = req.params;
 
-      const user = await User.findByPk(id);
-      return res.json(user);
+      const user = await User.findByPk(req.params.id);
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -34,15 +36,7 @@ class UserController {
 
   async update(req, res) {
     try {
-      const { id } = req.params;
-
-      if (!id) {
-        return res.status(400).json({
-          errors: ['ID não enviado'],
-        });
-      }
-
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
         return res.status(400).json({
@@ -51,7 +45,8 @@ class UserController {
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -61,24 +56,18 @@ class UserController {
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
-
-      if (!id) {
-        res.status(400).json({
-          errors: ['ID não enviado'],
-        });
-      }
-
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
 
       if (!user) {
-        res.status(400).json({
+        return res.status(400).json({
           errors: ['Usuário não encontrado'],
         });
       }
 
       await user.destroy();
-      return res.json('Usuário excluido com sucesso');
+      return res.json({
+        msg: 'Usuário excluido com sucesso',
+      });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (req, res, next) => {
+export default async function (req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization) {
@@ -13,10 +14,26 @@ export default (req, res, next) => {
 
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
-    const { id, email } = dados;
+    const { id, email } = dados; // Dados de payload
+    /* Checa na base de dados se os dados acima do payload do token são os mesmos dados do usuário.
+    */
+    /* Dessa forma, o usuário após alterar meu email ou id (no caso nao da) terá que relogar */
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        errors: ['Usuário Inválido'],
+      });
+    }
+
+    // Tanto o id quanto o email estarão disponiveis nas rotas depos desse middleware
     req.userId = id;
     req.userEmail = email;
-    // Tanto o id quanto o email estarão disponiveis nas rotas depos desse middleware
     return next();
   } catch (e) {
     console.log(e);
@@ -24,4 +41,4 @@ export default (req, res, next) => {
       errors: ['Token expirado ou inválido'],
     });
   }
-};
+}
